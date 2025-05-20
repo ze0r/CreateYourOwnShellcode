c6745a72ac6bbbedc415a77be3934829fd39a48e
ze0r <43227253+ze0r@users.noreply.github.com>
2025-05-20 16:20:24 +0800

    Update getopcode.py

--- getopcode.py
+++ getopcode.py
@@ -2,76 +2,78 @@ import re
 import pyperclip
 from keystone import *
 
-lineNumber = 0
-viewedASMCODE = ""
 
-def keystone_asm(code):
-    global viewedASMCODE, lineNumber
-
-    ks = Ks(KS_ARCH_X86, KS_MODE_64)
-    try:
-        encoding, count = ks.asm(code.split('//')[0])
-    except Exception as e:
-        print('Error occurred: %s line: %d ' %(code, lineNumber) )
-        return ('', 0)
-    lineNumber += 1
+def keystone_asm(code, lineNum):
     if (code.startswith('int')):
         encoding = [0xcc]
+    else:
+        ks = Ks(KS_ARCH_X86, KS_MODE_64)
+        try:
+            encoding, count = ks.asm(code.split('//')[0])
+        except Exception as e:
+            print('Error occurred: %s line: %d ' %(code, lineNum) )
+            return (b'', 0)
 
-    result = ''
-    for i in encoding:
-        result += '\\x%02x' % (i)
-    viewedASMCODE += result
-
-    result = '"%s" %s // %s \r\n' % (result," " * (50 - len(result)), code)
-
-    return (result, len(encoding))
+    return (bytes(encoding), len(encoding))
 
 ##################################################
 
 f = open('asm.txt', 'r')
-instructions = [ i.strip() for i in f.readlines() ]
+ins = [ i.strip() for i in f.readlines() ]
 f.close()
 
-current_instruction_position = 0
+lineNumber = 0
 label_list = {}
-for i in instructions:
+instructions =  []
+for i in ins:
     if i.startswith(':label'):
         label = i[1:].strip()
-        label_list.update({ label : current_instruction_position })
-        continue
-
-    if i.startswith('j'):
-        current_instruction_position += 2
-        label = re.search(' [A-Za-z0-9].+?( |$)', i).group(0).strip()
-        if (label_list.get(label)):
-            current_instruction_position += 4
-            if (i.startswith('jmp')):
-                current_instruction_position -= 1
+        instructions.append({'lineNum':lineNumber, 'instruction':'', 'opcode':b'', 'length':0, 'IsLabel':True, 'jmpInfo':False})
+        label_list.update({ label: lineNumber})
+    elif i.startswith('j'):
+        op = i[ : 3].strip()
+        label = i[3: ].strip()
+        if (i.startswith('jmp')):
+            instructions.append({'lineNum':lineNumber, 'instruction': i, 'opcode':b'\x90\x90\x90\x90\x90', 'length':5, 'IsLabel':False, 'jmpInfo':{'op': op, 'label':label, 'from': lineNumber, 'to': 0 } })
+        else:
+            instructions.append({'lineNum':lineNumber, 'instruction': i, 'opcode':b'\x90\x90\x90\x90\x90\x90', 'length':6, 'IsLabel':False, 'jmpInfo':{'op': op, 'label':label, 'from': lineNumber, 'to': 0 } })
     else:
-        opcode, length = keystone_asm(i)
-        current_instruction_position += length
+        opcode, length = keystone_asm(i, lineNumber)
+        instructions.append({'lineNum':lineNumber, 'instruction': i, 'opcode': opcode, 'length': length, 'IsLabel':False, 'jmpInfo':False})
+    lineNumber += 1
 
-result = ""
-viewedASMCODE = ""
-current_instruction_position = 0
 for i in instructions:
+    jmpInfo = i.get('jmpInfo')
+    if (jmpInfo): i['jmpInfo']['to'] = label_list.get(jmpInfo.get('label'))
 
-    if i.startswith(':label'): continue
-    if i.startswith('j'):
-        label = re.search(' [A-Za-z0-9].+?( |$)', i).group(0).strip()
-        pos =  label_list.get(label) - current_instruction_position
-        asm = i[:3] + '     ' + hex(pos & 0xffffffff)
-        opcode, length = keystone_asm(asm)
-    else:
-        opcode, length = keystone_asm(i)
+for i in range(len(instructions) - 1, 0, -1):
+    jmpInfo = instructions[i]['jmpInfo']
+    if (jmpInfo):
+        offset = 0
+        for x in range(jmpInfo.get('from'), jmpInfo.get('to'), 1):
+            offset += instructions[x]['length']
+        if ( (0 < offset) and (offset < 0x82) ):
+            offset -= 4
+            instructions[i]['length'] -= 4
+            if (jmpInfo.get('op') == 'jmp' ):
+                offset += 1
+                instructions[i]['length'] += 1
+            instructions[i]['opcode'] = b'\x90' * instructions[i]['length']
+        jmpInfo['to'] = offset
+        jmpInfo['from'] = 0
 
-    current_instruction_position += length
-    result += opcode
 
-result += "\"\\x90\";"
+for i in range(0, len(instructions), 1):
+    jmpInfo = instructions[i]['jmpInfo']
+    if (jmpInfo):
+        asm = jmpInfo.get('op') + ' ' + hex( jmpInfo.get('to') )
+        opcode, length = keystone_asm(asm, lineNumber)
+        instructions[i]['instruction'] = asm
+        instructions[i]['opcode'] = opcode
 
-print(result)
-pyperclip.copy(result)
+bincodex = b''
+for i in instructions:  bincodex += i['opcode']
 
-# for i in range(0, len(viewedASMCODE), 64): print('"%s"' % viewedASMCODE[i : i+64])
+f = open('ok.bin', 'wb')
+f.write(bincodex)
+f.close()
